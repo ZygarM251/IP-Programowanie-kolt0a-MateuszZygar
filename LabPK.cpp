99cac69af4e6f390532e9e3e2c1d7186da6121c5
ZygarM251 <115568775+ZygarM251@users.noreply.github.com>
2024-11-26 17:53:37 +0100

    Add files via upload

--- LabPK.cpp
+++ LabPK.cpp
@@ -8,94 +8,87 @@
 
 
 
-void testKonstruktorKopiujacyRozdzielaZasoby() 
+void testKonstruktorKopiujacyRozdzielaZasoby()
 {
-    cout << "Test Rozdzielania zasobow przez konstruktor kopujacy" << endl;
-    Zbior oryginal(10, 0.0, 100.0);
+	cout << "Test Rozdzielania zasobow przez konstruktor kopujacy" << endl;
+	Zbior oryginal(10, 0.0, 100.0);
 
-    Zbior kopia = oryginal;
-    if (false)
-    {
-        for (size_t i = 0; i < oryginal.liczbaElementow(); i++)
-        {
-            if (oryginal.getElement()[i] == kopia.getElement()[i] && oryginal.liczbaElementow() == kopia.liczbaElementow())
-            {
-                return;
-            }
+	Zbior kopia = oryginal;
 
-        }
-        cout << "FAIL"<<endl;
-    }
-    else
-    {
-        cout << "OK" << endl;
-    }
-    
+	for (size_t i = 0; i < oryginal.liczbaElementow(); i++)
+	{
+		if (oryginal.getElement()[i] == kopia.getElement()[i] && oryginal.liczbaElementow() == kopia.liczbaElementow())
+		{
+			cout << "OK" << endl;
+		}
+		else
+		{
+			cout << "FAIL" << endl;
+		}
 
+	}
 }
 
 
 void testOperatorPrzenoszacyPrzypisujeZasoby()
 {
-    cout << "Test Przypisania zasobu poprzez operator =" << endl;
-    Zbior oryginal(10, 0.0, 100.0);
+	cout << "Test Przypisania zasobu poprzez operator =" << endl;
+	Zbior oryginal(10, 0.0, 100.0);
 
-    Zbior przenoszenie = move(oryginal);
-    if (false)
-    {
-        for (size_t i = 0; i < oryginal.liczbaElementow(); i++)
-        {
-            if (przenoszenie.liczbaElementow() != oryginal.liczbaElementow())
-            {
-                return;
-            }
-            else if (przenoszenie.getElement()[i] != oryginal.getElement()[i])
-            {
-                return;
-            }
-        }
-        cout << "FAIL" << endl;
-    }
-    else
-    {
-        cout << "OK" << endl;
-    }
-    
+	Zbior przenoszenie = move(oryginal);
+
+	for (size_t i = 0; i < oryginal.liczbaElementow(); i++)
+	{
+		if (przenoszenie.liczbaElementow() != oryginal.liczbaElementow())
+		{
+			cout << "OK" << endl;
+		}
+		else if (przenoszenie.getElement()[i] != oryginal.getElement()[i])
+		{
+			cout << "OK" << endl;
+		}
+		else
+		{
+			cout << "Fail!" << endl;
+		}
+
+
+	}
 }
 
-void sprawdzPrawidloweZasoby(vector<Zbior>& zbiory)
-{
-    CzyPrawidlowyZasob predykat;
-    int prawidloweZasoby = 0;
+	void sprawdzPrawidloweZasoby(vector<Zbior>&zbiory)
+	{
+		CzyPrawidlowyZasob predykat;
+		int prawidloweZasoby = 0;
 
-    for (const auto& zbior : zbiory) 
-    {
-        if (predykat(zbior)) 
-        {
-            prawidloweZasoby++;
-        }
-    }
+		for (const auto& zbior : zbiory)
+		{
+			if (predykat(zbior))
+			{
+				prawidloweZasoby++;
+			}
+		}
 
-    cout << "Liczba instancji Zbioru z prawidlowym zasobem: " << prawidloweZasoby << endl;
-}
+		cout << "Liczba instancji Zbioru z prawidlowym zasobem: " << prawidloweZasoby << endl;
+	}
 
 
 
-int main()
-{
-    testKonstruktorKopiujacyRozdzielaZasoby();
-    testOperatorPrzenoszacyPrzypisujeZasoby();
+	int main()
+	{
+		testKonstruktorKopiujacyRozdzielaZasoby();
+		testOperatorPrzenoszacyPrzypisujeZasoby();
 
-    Zbior zbr1(10, 0.0, 100.0),zbr2(10, 0.0, 100.0), zbr3(10, 0.0, 100.0), zbr4(0, 0.0, 0.0);
-    Zbiory zbior;
+		Zbior zbr1(10, 0.0, 100.0), zbr2(10, 0.0, 100.0), zbr3(10, 0.0, 100.0), zbr4(0, 0.0, 0.0);
+		Zbiory zbior;
 
-    zbior.dodajZbior(zbr1);
-    zbior.dodajZbior(zbr2);
-    zbior.dodajZbior(zbr3);
-    zbior.dodajZbior(zbr4);
+		zbior.dodajZbior(zbr1);
+		zbior.dodajZbior(zbr2);
+		zbior.dodajZbior(zbr3);
+		zbior.dodajZbior(zbr4);
 
-    sprawdzPrawidloweZasoby(zbior);
-    
-}
+		/ sprawdzPrawidloweZasoby(zbior);
+
+	}
 
 #endif
